16dc6054a8bdded486feeaac2f91ce4e7cf4bfad
STS44 <iva25nova88@gmail.com>
2021-12-04 13:38:19 +0200

    Add file

---
+++ main.py
@@ -0,0 +1,43 @@
+# loops
+
+# task2 flip the string
+# s = str(input("Enter string: "))
+# while len(s) == 0:
+#     print("Incorrect Input")
+#     s = str(input("Enter string once more: "))
+# for i in range(len(s) - 1, -1, -1):
+#     print(s[i])
+
+# task 3 triangle
+# a = int(input("Enter height of the triangle: "))
+# if a > 1:
+#     for i in range(1, a+1):
+#         print("*"*i)
+#     for j in range(a-1, 0, -1):
+#         print(j * "*")
+# else:
+#     print("Incorrect input")
+#     exit(1)
+
+# task 4 sorting ASC and DESC
+# a = int(input("Enter integer a: "))
+# b = int(input("Enter integer b: "))
+# if a < b:
+#     for i in range(a, b+1):
+#         print(i)
+# elif a > b:
+#     for i in range(a, b-1, -1):
+#         print(i)
+# elif a == b:
+#     print("Incorrect input")
+#     exit(2)
+
+# task 5 output numbers
+# a = int(input("Enter integer a: "))
+# b = int(input("Enter integer b: "))
+# if a < b:
+#     for i in range(a, b+1):
+#         print(i, end=" ")
+# else:
+#     print("Incorrect input")
+#     exit(3)
