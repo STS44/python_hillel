f861af63242c387c68a97d50a11b3a29047e047f
STS44 <iva25nova88@gmail.com>
2021-12-14 00:05:30 +0200

    Changed variables

--- hw4.py
+++ hw4.py
@@ -1,42 +1,42 @@
 # loops
 
 # task 2 flip the string
-# s = str(input("Enter string: "))
-# while len(s) == 0:
+# myString = str(input("Enter string: "))
+# while len(myString) == 0:
 #     print("Incorrect Input")
-#     s = str(input("Enter string once more: "))
-# for i in range(len(s) - 1, -1, -1):
-#     print(s[i])
+#     myString = str(input("Enter string once more: "))
+# for i in range(len(myString) - 1, -1, -1):
+#     print(myString[i])
 
 # task 3 triangle
-# a = int(input("Enter height of the triangle: "))
-# if a > 1:
-#     for i in range(1, a+1):
+# side_a = int(input("Enter height of the triangle: "))
+# if side_a > 1:
+#     for i in range(1, side_a+1):
 #         print("*"*i)
-#     for j in range(a-1, 0, -1):
+#     for j in range(side_a-1, 0, -1):
 #         print(j * "*")
 # else:
 #     print("Incorrect input")
 #     exit(1)
 
 # task 4 sorting ASC and DESC
-# a = int(input("Enter integer a: "))
-# b = int(input("Enter integer b: "))
-# if a < b:
-#     for i in range(a, b+1):
+# num_a = int(input("Enter integer a: "))
+# num_b = int(input("Enter integer b: "))
+# if num_a < num_b:
+#     for i in range(num_a, num_b+1):
 #         print(i)
-# elif a > b:
-#     for i in range(a, b-1, -1):
+# elif num_a > num_b:
+#     for i in range(num_a, num_b-1, -1):
 #         print(i)
-# elif a == b:
+# elif num_a == num_b:
 #     print("Incorrect input")
 #     exit(2)
 
-# task 5 output numbers
-# a = int(input("Enter integer a: "))
-# b = int(input("Enter integer b: "))
-# if a < b:
-#     for i in range(a, b+1):
+# # task 5 output numbers
+# inp_a = int(input("Enter integer a: "))
+# inp_b = int(input("Enter integer b: "))
+# if inp_a < inp_b:
+#     for i in range(inp_a, inp_b+1):
 #         print(i, end=" ")
 # else:
 #     print("Incorrect input")
