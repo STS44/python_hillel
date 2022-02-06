2d405e6ac3759e856382a352bbb9d4a7bbd5b7a3
STS44 <iva25nova88@gmail.com>
2022-02-06 15:56:25 +0200

    coursework, some changes in read_data, clear_state, sell and buy usd functions

--- trader.py
+++ trader.py
@@ -5,21 +5,22 @@ from argparse import ArgumentParser
 
 # functions block
 
-def read_data_from_config(filename="config.json"):
-    with open(filename, "r") as config_json:
+def read_data_from_config_and_state():
+    with open("config.json", "r") as config_json:
         data = json.load(config_json)
-    return data
-
-def read_data_from_state(new_filename="state.json"):
-    with open(new_filename, "r") as state_json:
+    with open("state.json", "r") as state_json:
         new_data = json.load(state_json)
-    return new_data
+    return [data, new_data]
 
 def change_rate_and_write_state():
-    config_data = read_data_from_config(filename="config.json")
-    state_data = read_data_from_state(new_filename="state.json")
-    min_price = config_data["rate"] - config_data["delta"]
-    max_price = config_data["rate"] + config_data["delta"]
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
+    if "new_rate" not in state_data:
+        min_price = config_data["rate"] - config_data["delta"]
+        max_price = config_data["rate"] + config_data["delta"]
+    else:
+        min_price = state_data["new_rate"] - config_data["delta"]
+        max_price = state_data["new_rate"] + config_data["delta"]
     new_price = round(random.uniform(min_price, max_price), 2)
     with open("state.json", "w") as state_json:
         state_data["new_rate"] = new_price
@@ -27,75 +28,108 @@ def change_rate_and_write_state():
     return state_data
 
 def get_current_rate():
-    config_data = read_data_from_config(filename="config.json")
-    state_data = read_data_from_state(new_filename="state.json")
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
     if "new_rate" not in state_data:
         current_rate = config_data["rate"]
     else:
         current_rate = round(state_data["new_rate"], 2)
     print(f"{current_rate}")
 
-# ----------------------------------------buy and sell usd functions---------------------------------------------------
+# ----------------------------------------buy usd functions-----------------------------------------------------------
 
-def buy_usd_and_write_state(usd_amount):
-    config_data = read_data_from_config(filename="config.json")
-    state_data = read_data_from_state(new_filename="state.json")
+def get_available_uah_balance():
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
     if "new_uah_balance" not in state_data:
         available_uah_balance = config_data["uah_balance"]
     else:
         available_uah_balance = state_data["new_uah_balance"]
+    return available_uah_balance
+
+def calculate_required_uah_balance(usd_amount):
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
     if "new_rate" not in state_data:
         required_uah_balance = round(usd_amount * config_data["rate"], 2)
     else:
         required_uah_balance = round(usd_amount * state_data["new_rate"], 2)
+    return required_uah_balance
+
+def calculate_new_usd_balance_after_purchase(usd_amount):
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
+    if "new_uah_balance" not in state_data:
+        new_usd_balance = round(config_data["usd_balance"] + usd_amount, 2)
+    else:
+        new_usd_balance = round(state_data["new_usd_balance"] + usd_amount, 2)
+    return new_usd_balance
+
+def calculate_new_uah_balance_after_purchase(usd_amount):
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
+    if "new_uah_balance" not in state_data and "new_rate" not in state_data:
+        new_uah_balance = round(config_data["uah_balance"] - usd_amount * config_data["rate"], 2)
+    elif "new_uah_balance" not in state_data and "new_rate" in state_data:
+        new_uah_balance = round(config_data["uah_balance"] - usd_amount * state_data["new_rate"], 2)
+    elif "new_uah_balance" in state_data and "new_rate" not in state_data:
+        new_uah_balance = round(state_data["new_uah_balance"] - usd_amount * config_data["rate"], 2)
+    elif "new_uah_balance" in state_data and "new_rate" in state_data:
+        new_uah_balance = round(state_data["new_uah_balance"] - usd_amount * state_data["new_rate"], 2)
+    return new_uah_balance
+
+def buy_usd_and_write_state(usd_amount):
+    state_data = read_data_from_config_and_state()[1]
+    available_uah_balance = get_available_uah_balance()
+    required_uah_balance = calculate_required_uah_balance(usd_amount)
     if available_uah_balance < required_uah_balance:
         print(f"UNAVAILABLE, REQUIRED BALANCE UAH {required_uah_balance}, AVAILABLE {available_uah_balance}")
-
     else:
-        if "new_uah_balance" not in state_data:
-            new_usd_balance = round(config_data["usd_balance"] + usd_amount, 2)
-            if "new_rate" not in state_data:
-                new_uah_balance = round(config_data["uah_balance"] - usd_amount * config_data["rate"], 2)
-            else:
-                new_uah_balance = round(config_data["uah_balance"] - usd_amount * state_data["new_rate"], 2)
-        else:
-            new_usd_balance = round(state_data["new_usd_balance"] + usd_amount, 2)
-            if "new_rate" not in state_data:
-                new_uah_balance = round(state_data["new_uah_balance"] - usd_amount * config_data["rate"], 2)
-            else:
-                new_uah_balance = round(state_data["new_uah_balance"] - usd_amount * state_data["new_rate"], 2)
+        new_usd_balance = calculate_new_usd_balance_after_purchase(usd_amount)
+        new_uah_balance = calculate_new_uah_balance_after_purchase(usd_amount)
         with open("state.json", "w") as state_json:
             state_data["new_usd_balance"] = new_usd_balance
             state_data["new_uah_balance"] = new_uah_balance
             json.dump(state_data, state_json, indent=1)
     return state_data
 
-# --------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------sell usd functions---------------------------------------------------------
 
-def sell_usd_and_write_state(usd_amount):
-    config_data = read_data_from_config(filename="config.json")
-    state_data = read_data_from_state(new_filename="state.json")
+def get_available_usd_balance():
+    state_data = read_data_from_config_and_state()[1]
     if "new_usd_balance" not in state_data:
-        available_usd_balance = config_data["usd_balance"]
+        available_usd_balance = 0.00
     else:
         available_usd_balance = state_data["new_usd_balance"]
-    required_usd_balance = round(usd_amount, 2)
-    if available_usd_balance < required_usd_balance:
-        print(f"UNAVAILABLE, REQUIRED BALANCE USD {required_usd_balance}, AVAILABLE {available_usd_balance}")
+    return available_usd_balance
 
+def calculate_new_usd_balance_after_sale(usd_amount):
+    state_data = read_data_from_config_and_state()[1]
+    if "new_usd_balance" not in state_data:
+        new_usd_balance = 0.00
     else:
-        if "new_usd_balance" not in state_data:
-            new_usd_balance = round(config_data["usd_balance"] - usd_amount, 2)
-            if "new_rate" not in state_data:
-                new_uah_balance = round(config_data["uah_balance"] + usd_amount * config_data["rate"], 2)
-            else:
-                new_uah_balance = round(config_data["uah_balance"] + usd_amount * state_data["new_rate"], 2)
-        else:
-            new_usd_balance = round(state_data["new_usd_balance"] - usd_amount, 2)
-            if "new_rate" not in state_data:
-                new_uah_balance = round(state_data["new_uah_balance"] + usd_amount * config_data["rate"], 2)
-            else:
-                new_uah_balance = round(state_data["new_uah_balance"] + usd_amount * state_data["new_rate"], 2)
+        new_usd_balance = round(state_data["new_usd_balance"] - usd_amount, 2)
+    return new_usd_balance
+
+def calculate_new_uah_balance_after_sale(usd_amount):
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
+    if "new_usd_balance" not in state_data:
+        new_uah_balance = config_data["uah_balance"]
+    elif "new_usd_balance" in state_data and "new_rate" not in state_data:
+        new_uah_balance = round(state_data["new_uah_balance"] + usd_amount * config_data["rate"], 2)
+    elif "new_usd_balance" in state_data and "new_rate" in state_data:
+        new_uah_balance = round(state_data["new_uah_balance"] + usd_amount * state_data["new_rate"], 2)
+    return new_uah_balance
+
+def sell_usd_and_write_state(usd_amount):
+    state_data = read_data_from_config_and_state()[1]
+    available_usd_balance = get_available_usd_balance()
+    if available_usd_balance < usd_amount:
+        print(f"UNAVAILABLE, REQUIRED BALANCE USD {usd_amount}, AVAILABLE {available_usd_balance}")
+    else:
+        new_usd_balance = calculate_new_usd_balance_after_sale(usd_amount)
+        new_uah_balance = calculate_new_uah_balance_after_sale(usd_amount)
         with open("state.json", "w") as state_json:
             state_data["new_usd_balance"] = new_usd_balance
             state_data["new_uah_balance"] = new_uah_balance
@@ -105,8 +139,8 @@ def sell_usd_and_write_state(usd_amount):
 # --------------------------------------buy and sell all usd functions------------------------------------------------
 
 def buy_all_and_write_state():
-    state_data = read_data_from_state(new_filename="state.json")
-    config_data = read_data_from_config(filename="config.json")
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
     if "new_uah_balance" not in state_data and "new_rate" not in state_data:
         new_usd_balance = round(config_data["usd_balance"] + config_data["uah_balance"] / config_data["rate"], 2)
         new_uah_balance = 0.00
@@ -128,8 +162,8 @@ def buy_all_and_write_state():
     return state_data
 
 def sell_all_and_write_state():
-    state_data = read_data_from_state(new_filename="state.json")
-    config_data = read_data_from_config(filename="config.json")
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
     if "new_usd_balance" not in state_data and "new_rate" not in state_data:
         new_uah_balance = round(config_data["uah_balance"] + config_data["usd_balance"] * config_data["rate"], 2)
         new_usd_balance = 0.00
@@ -153,19 +187,17 @@ def sell_all_and_write_state():
 # --------------------------------------get balances and clear state.json---------------------------------------------
 
 def get_balances():
-    state_data = read_data_from_state(new_filename="state.json")
+    config_data = read_data_from_config_and_state()[0]
+    state_data = read_data_from_config_and_state()[1]
     if "new_uah_balance" in state_data and "new_usd_balance" in state_data:
         current_uah_balance = round(state_data["new_uah_balance"], 2)
         current_usd_balance = round(state_data["new_usd_balance"], 2)
     else:
-        config_data = read_data_from_config(filename="config.json")
         current_uah_balance = round(config_data["uah_balance"], 2)
         current_usd_balance = round(config_data["usd_balance"], 2)
     print(f"USD {current_usd_balance} UAH {current_uah_balance}")
 
 def clear_state():
-    state_data = read_data_from_state(new_filename="state.json")
-    state_data.clear()
     state_data = {}
     with open("state.json", "w") as state_json:
         json.dump(state_data, state_json, indent=1)
